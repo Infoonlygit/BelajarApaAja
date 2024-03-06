ab704bbbc6d4b961a688895e643f9c7acae03a60
Kevin Pandu Samudera (VinCrot) <162404229+Infoonlygit@users.noreply.github.com>
2024-03-06 11:56:40 +0700

    Update Testing.py

--- Testing.py
+++ Testing.py
@@ -2,11 +2,13 @@
 nama = input("Nama: myvintech")
 umur = input("Umur: 14 tahun")
 alamat = input("Alamat: Bata, Riau")
+email = input("Email: infoonly44@gmai.com")
 pekerjaan = input("Pekerjaan: Junior Programmer")
 
 # Menampilkan biodata
 print("\n=== Biodata ===")
 print(f"Nama     : {nama}")
 print(f"Umur     : {umur} tahun")
+print(f"email    : {email}")
 print(f"Alamat   : {alamat}")
 print(f"Pekerjaan: {pekerjaan}")

8663228aa194c75cec4926796597c03d7e0761f2
Infoonlygit <162404229+Infoonlygit@users.noreply.github.com>
2024-03-06 11:43:51 +0700

    Create Testing.py
    Intro testting.py membuat intro menggunakan python, teknik dasar (masih pemula)

---
+++ Testing.py
@@ -0,0 +1,12 @@
+# Input data
+nama = input("Nama: myvintech")
+umur = input("Umur: 14 tahun")
+alamat = input("Alamat: Bata, Riau")
+pekerjaan = input("Pekerjaan: Junior Programmer")
+
+# Menampilkan biodata
+print("\n=== Biodata ===")
+print(f"Nama     : {nama}")
+print(f"Umur     : {umur} tahun")
+print(f"Alamat   : {alamat}")
+print(f"Pekerjaan: {pekerjaan}")

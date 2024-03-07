5ee91f1ba7a54527d5774c9887ff1184774e6211
Kevin Pandu Samudera (VinCrot) <162404229+Infoonlygit@users.noreply.github.com>
2024-03-07 22:04:43 +0700

    Create docKPS.go
    Bio data ini dibuat dengan bahasa (Golang/Go)

---
+++ docKPS.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+    // Mendefinisikan variabel-variabel biodata
+    nama := "Kevin Pandu Samudera"
+    umur := "14 tahun"
+    alamat := "Batam, Riau"
+    pekerjaan := "Mahasiswa & Programmer"
+
+    // Menampilkan informasi biodata
+    fmt.Println("Nama:", nama)
+    fmt.Println("Umur:", umur, "tahun")
+    fmt.Println("Alamat:", alamat)
+    fmt.Println("Pekerjaan:", pekerjaan)
+}

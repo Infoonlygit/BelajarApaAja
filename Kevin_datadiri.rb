6412000b4601e8b028a153d113ddddfbafb3f884
Kevin Pandu Samudera (VinCrot) <162404229+Infoonlygit@users.noreply.github.com>
2024-03-07 21:06:10 +0700

    Create Kevin_datadiri.rb
    Data diri ini dibuat dengan ruby

---
+++ Kevin_datadiri.rb
@@ -0,0 +1,11 @@
+# Mendefinisikan variabel untuk data diri
+nama = "kevi pandu samudera"
+umur = 14 tahun
+alamat = "Bata, Riau"
+pekerjaan = "Mahasiswa"
+
+# Menampilkan informasi data diri
+puts "Nama: #{nama}"
+puts "Umur: #{umur} tahun"
+puts "Alamat: #{alamat}"
+puts "Pekerjaan: #{pekerjaan}"

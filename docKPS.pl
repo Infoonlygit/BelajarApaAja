f95fccd55666a67fea06bce59c1468631aa92fa6
Kevin Pandu Samudera (VinCrot) <162404229+Infoonlygit@users.noreply.github.com>
2024-03-07 22:11:34 +0700

    Create docKPS.pl
    Bio ini dibuat dengan bahasa (perl)

---
+++ docKPS.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+
+# Mendefinisikan variabel-variabel biodata
+my $nama = "Kevin Pandu Samudera";
+my $umur = "14 tahun";
+my $alamat = "Batam, Riau";
+my $pekerjaan = "Mahasisw, we dev";
+
+# Menampilkan informasi biodata
+print "Nama: $nama\n";
+print "Umur: $umur tahun\n";
+print "Alamat: $alamat\n";
+print "Pekerjaan: $pekerjaan\n";
